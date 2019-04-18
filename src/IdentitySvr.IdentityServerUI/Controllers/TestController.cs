3fd2ea1f65375cb4346bf0fc5e82cc406b24c3d2
marcKoutzarov <marc.koutzarov@gmail.com>
2019-04-18 06:28:48 +0200

    Added TestPage (still in progress)

--- src/IdentitySvr.IdentityServerUI/Controllers/TestController.cs
+++ src/IdentitySvr.IdentityServerUI/Controllers/TestController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using IdentitySvr.IdentityServerUI.Models;
+using IdentityModel.Client;
+using System.Net.Http;
 
 namespace IdentitySvr.IdentityServerUI.Controllers
 {
@@ -18,7 +20,44 @@ namespace IdentitySvr.IdentityServerUI.Controllers
 
 
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GetRefTokenAsync()
+        {
+            ReferenceTokenModel result = new ReferenceTokenModel();
 
+            IDiscoveryCache _discoCache = new DiscoveryCache("https://localhost:5001");
+            var disco = await _discoCache.GetAsync();
+
+            if (disco.IsError) {
+                // throw new Exception(disco.Error);
+                result.Result = disco.Error;
+
+                return View(result);
+            };
+
+            var client = new HttpClient();
+            var response = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "Client1",
+                ClientSecret = "Client1Secret",
+                UserName = "bob",
+                Password = "password"
+            });
+
+            if (response.IsError)
+            {
+                result.Result = response.Json.ToString();
+            }
+            else
+            {
+                result.Result = response.Json.ToString();
+            }
+
+            return View(result);
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -26,5 +65,8 @@ namespace IdentitySvr.IdentityServerUI.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
     }
 }
